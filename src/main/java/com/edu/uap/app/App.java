263b2a24c3ca02e700785372bbf9b22b0e2e6a5b
Marco Quenta Velasco <marcokani@gmail.com>
2022-02-06 23:53:03 -0400

    al compilar el archivo vars.properties no se genera

--- src/main/java/com/edu/uap/app/App.java
+++ src/main/java/com/edu/uap/app/App.java
@@ -1,4 +1,11 @@
 package com.edu.uap.app;
+import static spark.Spark.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * Hello world!
@@ -6,8 +13,13 @@ package com.edu.uap.app;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
+        Properties prop=new Properties();
+        // prop.load(new FileInputStream(new File("src/main/resources/var.properties")));
+        prop.load(new FileInputStream(new File("src/main/resources/vars.properties")));
         System.out.println( "Hello World!" );
+        System.out.println( "valor recuperado: "+prop.getProperty("PROPIEDAD_1"));
+        get("/tarea", (req, res) -> "En el ambiente xxxxxx");
     }
 }
